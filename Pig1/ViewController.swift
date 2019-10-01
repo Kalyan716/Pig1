cb44f43eaa200bbe9dd7852fb20f5f6863a5210b
Kalyan Tallapaneni <kalyan.t716@gmail.com>
2019-10-01 13:26:36 -0500

    ok

---
+++ Pig1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pig1
+//
+//  Created by Tallapaneni, Kalyan on 10/1/19.
+//  Copyright © 2019 Tallapaneni, Kalyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pig1Tests/Pig1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Pig1Tests.swift
+//  Pig1Tests
+//
+//  Created by Tallapaneni, Kalyan on 10/1/19.
+//  Copyright © 2019 Tallapaneni, Kalyan. All rights reserved.
+//
+
+import XCTest
+@testable import Pig1
+
+class Pig1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
