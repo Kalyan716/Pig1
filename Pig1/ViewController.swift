4b80cef38ce1851c01e7fdf896dfe9e40f8ad6f0
Kalyan Tallapaneni <kalyan.t716@gmail.com>
2019-10-03 13:08:29 -0500

    hehe

--- Pig1/ViewController.swift
+++ Pig1/ViewController.swift
@@ -13,10 +13,77 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var l = 1
-        var q = 0
+        view.backgroundColor = UIColor(red: 239/255, green: 115/255, blue: 193/255, alpha: 1.0)
+    }
+    var player1 = true
+    var T1 = 0
+    var R1 = 0
+    var T2 = 0
+    var R2 = 0
+    var sad = "sorry, you rolled a one"
+
+    @IBOutlet var TScore1: UILabel!
+    
+    @IBOutlet var RScore1: UILabel!
+    
+    @IBOutlet var TScore2: UILabel!
+    
+    @IBOutlet var RScore2: UILabel!
+    
+    @IBOutlet var numRolled: UILabel!
+    
+    @IBOutlet var result: UILabel!
+    
+    func randNum() -> Int {
+        var randomNum = Int.random(in: 1...6)
+        return randomNum
+        
+    }
+    
+    @IBAction func roll(_ sender: Any) {
+        let rolled = randNum()
+        numRolled.text = String(rolled)
+        if player1 {
+            if rolled == 1 {
+                R1 = 0
+                player1 = false
+            }
+            else {
+                R1 = R1 + rolled
+                T1 = T1 + rolled
+                if T1 >= 100 {
+                    result.text = "Player 1 wins!"
+                }
+                TScore1.text = String(T1)
+                RScore1.text = String(R1)
+            }
+        }
+        else {
+            if rolled == 1 {
+                R2 = 0
+                player1 = true
+            }
+            else {
+                R2 = R2 + rolled
+                T2 = T2 + rolled
+                if T2 >= 100 {
+                    result.text = "Player 2 wins!"
+                }
+                TScore2.text = String(T2)
+                RScore2.text = String(R2)
+            }
+        }
+    }
+        func stop1 () {
+            if player1 == true {
+                player1 = false
+            }
+            else {
+                player1 = true
+            }
+        }
+    @IBAction func stop(_ sender: Any) {
+        stop1()
     }
 
-
 }
-
